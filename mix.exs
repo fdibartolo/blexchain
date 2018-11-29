95823b19ba1cc618b51294b9396b7e5fadfae9e3
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2018-11-29 16:37:10 -0300

    remove coveralls warning message

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule Blexchain.Mixfile do
       start_permanent: Mix.env == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]      
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]      
     ]
   end
 
